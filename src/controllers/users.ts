5c9f6dba0e6d0b50599b280338337d24c1564eec
rizqisamawa <rizqisamawa11@gmail.com>
2024-02-08 16:17:23 +0700

    feat: delete & update users

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { getUsers } from "../db/users";
+import { deleteUserById, getUserById, getUsers } from "../db/users";
 
 export const getAllUsers = async (
   req: express.Request,
@@ -10,6 +10,50 @@ export const getAllUsers = async (
     const users = await getUsers();
 
     return res.status(200).json(users);
+  } catch (err) {
+    console.log(err);
+    return res.sendStatus(400);
+  }
+};
+
+export const deleteUser = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const { id } = req.params;
+
+    const deletedUser = await deleteUserById(id);
+
+    return res.json(deletedUser);
+  } catch (err) {
+    console.log(err);
+    return res.sendStatus(400);
+  }
+};
+
+export const updateUser = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const { id } = req.params;
+    const { username } = req.body;
+
+    if (!username) {
+      return res.sendStatus(400);
+    }
+
+    const user = await getUserById(id);
+
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+
+    user.username = username;
+    await user.save();
+
+    return res.status(200).json(user).end();
   } catch (err) {
     console.log(err);
     return res.sendStatus(400);
